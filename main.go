b748c2c6621d324239b1417bc42e6a09f93e5417
Jeson <jeson.suryadinata@yahoo.com>
2023-08-14 10:50:13 +0700

    Initial commit

---
+++ main.go
@@ -0,0 +1,24 @@
+package main
+
+import (
+	"log"
+	"os"
+
+	"github.com/gofiber/fiber/v2"
+)
+
+func main() {
+	app := fiber.New()
+
+	app.get("/", func(c *fiber.Ctx) error {
+		return c.SendString("Hello world !")
+	})
+
+	port := os.Getenv("PORT")
+
+	if port == "" {
+		port = "3000"
+	}
+
+	log.Fatal(app.Listen("0.0.0.0:" + port))
+}
